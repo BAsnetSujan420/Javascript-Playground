bdae616f8a7a0a13fd91ae79ec7b2308a935773f
BAsnetSujan420 <basnetsujan1828420@gmail.com>
2021-05-25 19:22:05 +0545

    Updated play/pause icon when clicked

--- Javascript30/11 - Custom Video Player/scripts.js
+++ Javascript30/11 - Custom Video Player/scripts.js
@@ -13,6 +13,15 @@ function togglePlay() {
     video[method]();
   }
 
+  function updateButton() {
+    const icon = this.paused ? '►' : '❚ ❚';
+    console.log(icon);
+    toggle.textContent = icon;
+  }
+  
 /*Hook up the event listeners */ 
 video.addEventListener('click', togglePlay);
 toggle.addEventListener('click', togglePlay);
+
+video.addEventListener('play', updateButton);
+video.addEventListener('pause', updateButton);
