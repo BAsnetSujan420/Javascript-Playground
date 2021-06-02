9577ef9343e7ff2ee3577cff35f0a56149009c87
BAsnetSujan420 <basnetsujan1828420@gmail.com>
2021-06-02 15:10:47 +0545

    Added filter effect in Canvas video

--- Javascript30/19 - Webcam Fun/scripts.js
+++ Javascript30/19 - Webcam Fun/scripts.js
@@ -23,7 +23,14 @@ function paintToCanvas(){
     canvas.height = height;
 
     return setInterval(() => {
-ctx.drawImage(video, 0 , 0, width, height);
+    ctx.drawImage(video, 0 , 0, width, height);
+     // take the pixels out 
+    let pixels = ctx.getImageData(0, 0, width, height);
+  // mess with them
+     pixels = filterEffect(pixels);
+
+     // put them back
+     ctx.putImageData(pixels, 0, 0);
     }, 16);
 }
 
@@ -41,6 +48,14 @@ function takePhoto(){
     strip.insertBefore(link, strip.firstChild);
 }
 
+function filterEffect(pixels) {
+    for (let i = 0; i < pixels.data.length; i += 4) {
+      pixels.data[i + 0] = pixels.data[i + 0] - 100; // RED
+      pixels.data[i + 1] = pixels.data[i + 1] - 50; // GREEN
+      pixels.data[i + 2] = pixels.data[i + 2] * 0.5; // Blue
+    }
+    return pixels;
+  }
 
 getVideo()
 
